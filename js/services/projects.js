370ae946d2c6fa24c530036d849bdf98795662fb
mbernardes <mrsbernardes@gmail.com>
2015-11-20 15:33:55 +0000

    Changes JSON from dropbox to GIT
    Waaaaay faster!

--- js/services/projects.js
+++ js/services/projects.js
@@ -1,5 +1,5 @@
  app.factory('projects', ['$http', function($http) { 
-  return $http.get('https://dl.dropboxusercontent.com/u/4353431/projects.json') 
+  return $http.get('https://gist.githubusercontent.com/mbernardes/eed7691aada463988d2e/raw/9e9c5b0b515acff8739bcf2efc4f6b2abfc85c7c/projects.json') 
             .success(function(data) { 
               return data; 
             }) 
